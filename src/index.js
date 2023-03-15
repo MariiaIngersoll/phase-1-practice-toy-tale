686e2dc4d4a659be0bcfa4534ecf875ebc409309
+ <+>
2023-03-14 23:02:05 -0400

    done

--- src/index.js
+++ src/index.js
@@ -13,3 +13,43 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+
+const toyCollectionDiv = document.querySelector("#toy-collection")
+
+
+fetch("http://localhost:3000/toys")
+.then(responce => responce.json())
+.then((somedata) => {
+  
+  const arrayOfToyObjects  = somedata
+  console.log(arrayOfToyObjects)
+
+  arrayOfToyObjects.map( (eachToyObject) => {
+     const divForToyCard = document.createElement("div")
+           divForToyCard.className = "card"
+     const h2ForToyCard = document.createElement("h2")
+           h2ForToyCard.textContent = eachToyObject.name 
+     const imageForToyCard = document.createElement("img")
+           imageForToyCard.src = eachToyObject.image
+           imageForToyCard.className = "toy-avatar" 
+     const pTagForToyCard = document.createElement('p')
+           pTagForToyCard.textContent = `${eachToyObject.likes} likes `
+     const buttonForToyCard = document.createElement("button")
+           buttonForToyCard.innerText = "Like <3"
+           buttonForToyCard.className = "like-bnt"
+           buttonForToyCard.id = eachToyObject.id
+           buttonForToyCard.addEventListener("click", () => console.log("Increment those likes"))
+     
+
+    divForToyCard.append(
+      h2ForToyCard , 
+      imageForToyCard ,
+      pTagForToyCard ,
+      buttonForToyCard
+    )
+    toyCollectionDiv.append(divForToyCard)
+  })
+})
+
